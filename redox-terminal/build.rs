d9fb8e30db23df12b399506e832caf30526dc7a5
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-01-02 19:51:53 +0000

    More changes related to the restructure.

--- redox-terminal/build.rs
+++ redox-terminal/build.rs
@@ -1,17 +1,9 @@
-#[cfg(windows)]
-extern crate winres;
-
 fn main() {
-    let sub_dir = if cfg!(debug_assertions) {
+    let _sub_dir = if cfg!(debug_assertions) {
         "debug"
     } else {
         "release"
     };
-
-    // Copy the paths.json file to the correct build directory.
-    if std::fs::copy("paths.json", format!("target/{sub_dir}/paths.json")).is_err() {
-        eprintln!("Error attempting to copy the paths JSON file.");
-    }
 
     #[cfg(windows)]
     windows_only();
@@ -19,6 +11,8 @@ fn main() {
 
 #[cfg(windows)]
 fn windows_only() {
+    extern crate winres;
+
     /*let mut res = winres::WindowsResource::new();
     res.set_icon("..//assets//icon.ico");
     res.compile().unwrap();*/
